3d1919bc59eb91b5e120647db005869eeb3da4ea
parcivad <68159486+parcivad@users.noreply.github.com>
2022-09-30 12:51:25 +0200

    Update studentRegister.js

--- js/studentRegister.js
+++ js/studentRegister.js
@@ -75,7 +75,7 @@ function registerUser() {
             let isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
             if(isAndroid) {
                 // open with google calender
-                location.assign(`https://calendar.google.com/calendar/u/0/r?cid=${data.replace('"', '').replace('"', "")}&pli=1`)
+                location.assign(`https://calendar.google.com/calendar/u/0/r?cid=https://${apiDomain}/student/feed?studentId=${data.replace('"', '').replace('"', "")}&pli=1`)
             } else {
                 // open with apple calendar
                 location.assign(`webcal://${apiDomain}/student/feed?studentId=${data.replace('"', '').replace('"', "")}`)
