45aa203c8c4535c7a6a5ad7128f150bbfe169815
parcivad <68159486+parcivad@users.noreply.github.com>
2022-09-30 13:00:10 +0200

    Update studentRegister.js

--- js/studentRegister.js
+++ js/studentRegister.js
@@ -13,6 +13,14 @@ function loadCourse() {
                 // create html element and push it under the playerlist div
                 $("#courseSelector").append(`<option value="${course["courseId"]}">${course["courseName"]}</option>`)
             })
+        
+            var options = $('#courseSelector option');
+            var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
+            arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
+            options.each(function(i, o) {
+              o.value = arr[i].v;
+              $(o).text(arr[i].t);
+            });
 
             // end loading animation
             animateStop("registerView")
